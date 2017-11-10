0f2b8fa17ece5411fb769078360f56c9166e4695
sujinleeme <sujinlee.me@gmail.com>
2017-11-10 20:41:02 +0900

    Update menu action

--- readable/src/modules/actions/menu.js
+++ readable/src/modules/actions/menu.js
@@ -9,52 +9,39 @@ export const SELECT_TAB = 'SELECT_TAB'
 export const SETUP_MENU_SUCCESS = 'SETUP_MENU_SUCCESS'
 export const CHANGE_EDIT_MENU = 'CHANGE_EDIT_MENU'
 
-
-
-export const setupMenuComplete = (bool) => {
+export const setupMenuSuccess = (bool) => {
   return {
-    type: SETUP_MENU_SUCCESS,
-    hasloaded: bool,
-  }
-}
-
-export const setupMenu = (categoryName, tabName) => {
-  return (dispatch) => {
-     Promise.all([
-      dispatch(push(`/category/${categoryName}?=${tabName}`)),
-      dispatch(selectCategory({category: categoryName})),
-      dispatch(selectTab({tab: tabName}),
-      )]).then((response) => {
-      dispatch(setupMenuComplete(true))
-    }).catch((failure) => dispatch(setupMenuComplete(false)))
+    type: SETUP_MENU_SUCCESS, hasloaded: bool,
   }
 }
 
 export const selectCategory = ({category}) => {
   return {
-    type: SELECT_CATEGORY,
-    category,
+    type: SELECT_CATEGORY, category,
   }
 }
 
 export const selectTab = ({tab}) => {
   return {
-    type: SELECT_TAB,
-    tab,
+    type: SELECT_TAB, tab,
   }
 }
 
 export const fetchCategoryDataSuccess = (categories) => {
   return {
-    type: FETCH_CATEGORY_DATA_SUCCESS,
-    categories,
+    type: FETCH_CATEGORY_DATA_SUCCESS, categories,
+  }
+}
+
+export const fetchTabDataSuccess = (tabs) => {
+  return {
+    type: FETCH_TAB_DATA_SUCCESS, tabs,
   }
 }
 
 export const getCategories = () => {
   return (dispatch) => {
-    fetch(`${baseurl}/categories`, {headers}).
-    then((response) => {
+    fetch(`${baseurl}/categories`, {headers}).then((response) => {
       if (!response.ok) {
         throw Error(response.statusText)
       }
@@ -64,10 +51,14 @@ export const getCategories = () => {
   }
 }
 
-export const fetchTabDataSuccess = (tabs) => {
-  return {
-    type: FETCH_TAB_DATA_SUCCESS,
-    tabs,
+export const setupMenu = (categoryName, tabName) => {
+  return (dispatch) => {
+    Promise.all([
+      dispatch(selectCategory({category: categoryName})),
+      dispatch(selectTab({tab: tabName}),),
+    ]).then((response) => {
+      dispatch(setupMenuSuccess(true))
+    }).catch((failure) => dispatch(setupMenuSuccess(false)))
   }
 }
 
@@ -82,12 +73,5 @@ export const getTabs = () => {
     })
     .then((response) => response.json())
     .then((data) => dispatch(fetchTabDataSuccess(data.tabs)))
-  }
-}
-
-export const changeEditView = (bool) => {
-  return {
-    type: CHANGE_EDIT_MENU,
-    isEditing: bool,
   }
 }
