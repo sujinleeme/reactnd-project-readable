48dd77b4acc26395e998f12d7a1e504f25096f50
sujinleeme <sujinlee.me@gmail.com>
2017-11-13 19:53:16 +0900

    Code Refactoring : Show the number of comments on every post

---
+++ readable/src/components/container/AllPostsContainer.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { withStyles } from 'material-ui/styles'
+import { connect } from 'react-redux'
+import {
+  withRouter, Link,
+} from 'react-router-dom'
+
+import PostCardList from '../post/body/PostCardList'
+
+import NewPost from '../post/create/NewPost'
+import LoadingProgress from '../assests/LoadingProgress'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.A300,
+  },
+  
+})
+
+class PostListContainer extends React.Component {
+  componentUnMount() {
+  
+  }
+  
+  render () {
+    const {classes, postList, currentCategory, currentTab} = this.props
+    const {loading} = postList
+    const posts = postList.posts
+    if (loading) {
+      
+      return (
+        <div className={classes.root}>
+          <NewPost/>
+          <LoadingProgress/>
+        </div>
+      )
+    }
+    return (
+      
+      <div className={classes.root}>
+        <NewPost/>
+        {posts ? posts.map((post) => (
+          <Link
+            key={post.id}
+            to={{
+              pathname: `/category/${currentCategory}=${currentTab}/posts/${post.id}`,
+              state: {
+                category: currentCategory, tab: currentTab, id: post.id,
+              },
+            }}
+          >
+            <PostCardList
+              post={post}
+              id={post.id}
+              key={post.id}
+            />
+          </Link>
+        
+        )) : null}
+      </div>
+    
+    )
+  }
+}
+
+PostListContainer.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) => {
+  return {
+    postList: state.posts.postList,
+    currentCategory: state.currentMenu.category,
+    currentTab: state.currentMenu.tab,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  
+  }
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withRouter(withStyles(styles)(PostListContainer)))
